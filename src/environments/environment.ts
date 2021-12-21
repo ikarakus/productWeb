603c2c010c8338a1311c05d4a6120b1cce5117e3
neoland <ikarakus@gmail.com>
2021-12-21 18:15:26 +0300

    assignment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,5 @@ export const environment = {
   s3Url: 'https://content-guru.s3.eu-west-1.amazonaws.com',
   region: 'eu-west-1',
   userPoolId: 'eu-west-1_230G04J9o',
-  userPoolWebClientId: '4vp9o18i4f72fvd13it08kqfbi',
-  file_size : 20000000 // max 20 MB
+  userPoolWebClientId: '4vp9o18i4f72fvd13it08kqfbi'
 };
