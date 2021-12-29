563138b64040c0b376ae7defb186d1c1bdcbaf6d
neoland <ikarakus@gmail.com>
2021-12-29 11:12:35 +0300

    assignment

--- src/app/product/product-routing.module.ts
+++ src/app/product/product-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ProductListComponent} from './product-list/product-list.component';
+import {ProductDetailComponent} from './product-detail/product-detail.component';
 
 
-const routes: Routes = [  {path : '', component: ProductListComponent}];
+const routes: Routes = [
+  {path : '', component: ProductListComponent},
+  {path: 'product-detail',component: ProductDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
