0d711ee3793899fcff693b6a8860c29effd78270
neoland <ikarakus@gmail.com>
2022-01-02 11:02:26 +0300

    assignment

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -14,7 +14,6 @@ export class ProductListComponent implements OnInit {
 
   productList$:Observable<Product[]>;
   reviewList: Review[];
-  public averageRate: number;
   constructor(private service:ProductService) {}
 
   ngOnInit() {
@@ -27,13 +26,11 @@ export class ProductListComponent implements OnInit {
 
   public calculateRate(id: number) {
     this.reviewList = UtilsGeneral.getReviewList();
-    this.averageRate = 0;
     if (this.reviewList) {
       this.reviewList = this.reviewList.filter(item => item.productId===id);
       if (this.reviewList.length > 0 ) {
-        this.averageRate = this.reviewList.reduce((sum, current) => sum + current.rate, 0) / this.reviewList.length;
+        return this.reviewList.reduce((sum, current) => sum + current.rate, 0) / this.reviewList.length;
       }
     }
-    return this.averageRate;
   }
 }
